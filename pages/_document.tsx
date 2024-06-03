d967f2f79dee53d42f7ee6b01e368d9933f7b66c
Kim Ji Yu <spbabo97@naver.com>
2024-06-04 05:33:19 +0900

    :sparkles: 카카오맵 추가

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,10 @@ export default function Document() {
       <body className="min-h-screen">
         <Main />
         <NextScript />
+        <Script
+          src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_JAVASCRIPT}&libraries=services,clusterer&autoload=false`}
+          strategy="beforeInteractive"
+        />
       </body>
     </Html>
   );
--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import {Button, ButtonGroup} from "@nextui-org/button";
 import { signOut } from 'next-auth/react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import {Badge} from "@nextui-org/badge";
+import { Avatar } from '@nextui-org/react';
 
 export default function Home() {
   return (
-    <div>
-        <Button onClick={()=>signOut({callbackUrl : "/"})}>로그아웃</Button>
+    <div className='h-full relative'>
+      <Map
+        center={{ lat: 33.5563, lng: 126.79581 }}
+        style={{ width: "100%", height: "100%" }}
+      >
+      </Map>
+      <div className="py-8 p-4 absolute w-full z-10 bottom-0 left-0 bg-white rounded-2xl flex gap-3">
+        <Avatar radius='md' size="lg"/>
+        <dl>
+          <dt className="text-base font-bold">가게이름</dt>
+          <dd className='text-xs mt-2'>등록날짜</dd>
+        </dl>
+      </div>
     </div>
   )
 }
+
+// {/* <Button onClick={()=>signOut({callbackUrl : "/"})}>로그아웃</Button> */}
